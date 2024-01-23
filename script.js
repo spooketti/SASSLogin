e3bc69a202d7656c143602798a7d0ba58665c8bb
spooketti <jonliu168@gmail.com>
2024-01-23 14:30:44 -0800

    we aer so screwed

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ req.addEventListener("load", function(){
     document.getElementById("BG").style.backgroundImage = `url(${response.url})`
   }
 })
+
+function sendLogin(un,pw)
+{
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://f3bbc04a-027d-4662-b0c3-314fd8a29b82-00-36k060m6zrig0.spock.replit.dev/api/login");
+    xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    const body = JSON.stringify({
+      "username": un,
+      "password": crap 
+    });
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(JSON.parse(xhr.responseText));
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+    xhr.send(body);
+}
